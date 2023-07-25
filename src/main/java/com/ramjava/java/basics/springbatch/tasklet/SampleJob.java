8806c512053353b92c367316669cfb1421120e96
Romorosa Ramjay (ERNI) <ramjay.romorosa@betterask.erni>
2023-07-25 10:47:23 +0800

    added spring batch tasklet

--- src/main/java/com/ramjava/java/basics/springbatch/tasklet/SampleJob.java
+++ src/main/java/com/ramjava/java/basics/springbatch/tasklet/SampleJob.java
@@ -1,2 +1,45 @@
-package com.ramjava.java.basics.springbatch.tasklet;public class SampleJob {
+package com.ramjava.java.basics.springbatch.tasklet;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SampleJob {
+    // Create the job
+    @Autowired
+    private JobBuilderFactory jobBuilderFactory;
+    // Create the step
+    @Autowired
+    private StepBuilderFactory stepBuilderFactory;
+    @Bean
+    public Job firstJob() {
+        return jobBuilderFactory.get("First Job")
+                .start(firstStep())
+                .build();
+    }
+
+    private Step firstStep() {
+        return stepBuilderFactory.get("First Step")
+                .tasklet(firstTask())
+                .build();
+    }
+
+    private Tasklet firstTask() {
+        return new Tasklet() {
+            @Override
+            public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+                System.out.println("This is first tasklet step");
+                return RepeatStatus.FINISHED;
+            }
+        };
+    }
 }
